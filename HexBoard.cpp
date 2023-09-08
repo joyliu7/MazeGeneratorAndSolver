0c18bca44d3627486c697c89d0075e4964f271c2
Joy Liu <141117721+joyliu7@users.noreply.github.com>
2023-09-07 21:10:16 -0700

    Add files via upload

---
+++ HexBoard.cpp
@@ -0,0 +1,36 @@
+#include "HexBoard.h"
+
+#include <iostream>
+
+#include "queue.h"
+#include "stack.h"
+
+HexBoard::HexBoard(int radius) {
+    /**
+     * @todo Your code here!
+     */
+}
+
+void HexBoard::create_board() {
+    /**
+     * @todo Your code here!
+     */
+}
+
+void HexBoard::configure_cells() {
+    /**
+     * @todo Your code here!
+     */
+}
+
+void HexBoard::generate_maze(pair<int, int> start_coord, double branch_prob) {
+    /**
+     * @todo Your code here!
+     */
+}
+
+void HexBoard::solve_maze() {
+    /**
+     * @todo Your code here!
+     */
+}
---
+++ HexCell.cpp
@@ -0,0 +1,43 @@
+#include "HexCell.h"
+
+#include <cmath>
+
+HexCell::HexCell(int Q, int R) {
+    /**
+     * @todo Your code here!
+     */
+}
+
+void HexCell::link(HexCell* cell) {
+    /**
+     * @todo Your code here!
+     */
+}
+
+int HexCell::get_neighbour_side(const HexCell* cell) const {
+    /**
+     * @todo Your code here! You will need to replace the following line.
+     */
+    return -1;
+}
+
+bool HexCell::is_linked(const HexCell* cell) const {
+    /**
+     * @todo Your code here! You will need to replace the following line.
+     */
+    return false;
+}
+
+int HexCell::distance(const HexCell* cell) const {
+    /**
+     * @todo Your code here! You will need to replace the following line.
+     */
+    return -1;
+}
+
+bool HexCell::is_edge() const {
+    /**
+     * @todo Your code here! You will need to replace the following line.
+     */
+    return false;
+}
